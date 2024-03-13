c1a36b254743d40b1b8a9df4f595bc3b700e2e14
uiyeon831 <104365642+uiyeon831@users.noreply.github.com>
2024-03-13 15:07:46 +0900

    backend 기본 설정
    backend 기본 설정

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const port = 4000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello');
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('req.body');
+})
+
+//정적인 파일을 불러올 수 있도록
+app.use(express.static(path.join(__dirname, '../uploads')));
+
 app.listen(port, () => {
-  console.log(`${port}번에서 실행이 되었습니다.`);
+  console.log(`
+############################
+#     Server is start!     #
+############################`);
 });
