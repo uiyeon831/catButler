6f323bb247319d44308fbf85f0d44c09b4f22192
uiyeon831 <104365642+uiyeon831@users.noreply.github.com>
2024-03-28 15:56:09 +0900

    feat: header icon hover event
    feat: header icon hover event

--- frontend/src/layout/Header/index.jsx
+++ frontend/src/layout/Header/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 import * as Styled from './style';
 
@@ -8,8 +9,68 @@ import LikeIcon from '../../components/icons/LikeIcon';
 import CartIcon from '../../components/icons/CartIcon';
 import PersonIcon from '../../components/icons/PersonIcon';
 import SearchIcon from '../../components/icons/SearchIcon';
+import ArrowIcon from './../../components/icons/ArrowIcon';
+import { Btn } from '../../components/style';
 
 export default function Header() {
+  const [isHover, setIsHover] = useState(false);
+  const [isToken, setIsToken] = useState(false);
+
+
+  useEffect(() => {
+    const localToken = localStorage.getItem('Token');
+    const sessoionToken = sessionStorage.getItem('Token');
+    if(localToken || sessoionToken) {
+      setIsToken(true);
+    } else {
+      setIsToken(false);
+    }
+  },[isHover])
+
+  //token이 있을 때 hover이벤트에 보여질 내용
+  const withTokenHover = <>
+    <div className='textBox'>
+      <p>최근 본 상품</p>
+      <ArrowIcon width='10' />
+    </div>
+    <div className='textBox'>
+      <p>주문 / 배송 조회</p>
+      <ArrowIcon width='10' />
+    </div>
+    <div className='textBox'>
+      <p>찜 리스트</p>
+      <ArrowIcon width='10' />
+    </div>
+    <div className='textBox'>
+      <p>장바구니</p>
+      <ArrowIcon width='10' />
+    </div>
+    <div className='textBox'>
+      <p>1 : 1 문의</p>
+      <ArrowIcon width='10' />
+    </div>
+  </>
+
+  //token이 없을 때 hover이벤트에 보여질 내용
+  const withoutTokenHover = <>
+    <div className='textContainer'>
+      <p>로그인 후</p>
+      <p>이용 가능한 서비스입니다.</p>
+    </div>
+    <div className='lineContainer'><div className='line' /></div>
+    <div className='btnContainer'>
+      <Link to={'/login'}><Btn>로그인</Btn></Link>
+      <Link to={'/join'}><Btn>회원가입</Btn></Link>
+    </div>
+  </>
+
+  //hover이벤트에 보여질 내용
+  const hoverContent = <Styled.HoverContainer>
+    <div className='hoverBox'>
+      {isToken ? withTokenHover : withoutTokenHover}
+    </div>
+  </Styled.HoverContainer>
+
   return (
     <Styled.headerContainer>
       <Link to={'/'}><CatButlerLogo width={170} /></Link>
@@ -22,7 +83,13 @@ export default function Header() {
       <div className='iconBox'>
         <Link><LikeIcon width='38' /></Link>
         <Link><CartIcon width='36' /></Link>
-        <Link><PersonIcon width='35' /></Link>
+        <div className='personIconBox'
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          <PersonIcon width='35' />
+          {isHover ? hoverContent : null}
+        </div>
       </div>
     </Styled.headerContainer>
   )
